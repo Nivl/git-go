a0d0d5abbb7deff85e7dca826455cc21aea69c65
Melvin Laplanche <melvin.wont.reply@gmail.com>
2020-06-23 22:41:32 -0700

    add ests for repo.GetObject

--- repo_test.go
+++ repo_test.go
@@ -6,7 +6,10 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/Nivl/git-go/plumbing"
+	"github.com/Nivl/git-go/plumbing/object"
 	"github.com/Nivl/git-go/testhelper"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -70,4 +73,49 @@ func TestLoad(t *testing.T) {
 	// assert returned repository
 	require.Equal(t, repoPath, r.projectPath)
 	require.Equal(t, filepath.Join(repoPath, DotGitPath), r.path)
+}
+
+func TestGetObject(t *testing.T) {
+	t.Parallel()
+
+	t.Run("dangling object", func(t *testing.T) {
+		t.Parallel()
+
+		repoPath, cleanup := testhelper.UnTar(t, testhelper.RepoSmall)
+		defer cleanup()
+
+		r, err := LoadRepository(repoPath)
+		require.NoError(t, err, "failed loading a repo")
+
+		oid, err := plumbing.NewOidFromStr("b07e28976ac8972715598f390964d53cf4dbc1bd")
+		require.NoError(t, err)
+
+		obj, err := r.GetObject(oid)
+		require.NoError(t, err)
+		require.NotNil(t, obj)
+
+		assert.Equal(t, oid, obj.ID)
+		assert.Equal(t, object.TypeBlob, obj.Type())
+		assert.Equal(t, "package packfile", string(obj.Bytes()[:16]))
+	})
+
+	t.Run("Object from packfile", func(t *testing.T) {
+		t.Parallel()
+
+		repoPath, cleanup := testhelper.UnTar(t, testhelper.RepoSmall)
+		defer cleanup()
+
+		r, err := LoadRepository(repoPath)
+		require.NoError(t, err, "failed loading a repo")
+
+		oid, err := plumbing.NewOidFromStr("1dcdadc2a420225783794fbffd51e2e137a69646")
+		require.NoError(t, err)
+
+		obj, err := r.GetObject(oid)
+		require.NoError(t, err)
+		require.NotNil(t, obj)
+
+		assert.Equal(t, oid, obj.ID)
+		assert.Equal(t, object.TypeCommit, obj.Type())
+	})
 }
