65c58da98680e5369d8cdbc5e601d0b552c9f66a
Melvin Laplanche <melvin.wont.reply@gmail.com>
2020-12-27 23:35:30 -0800

    fix linter

--- internal/errutil/errutil.go
+++ internal/errutil/errutil.go
@@ -4,7 +4,7 @@ package errutil
 import "io"
 
 // Close closes the closer and sets the error to err if err is nil
-func Close(c io.Closer, err *error) {
+func Close(c io.Closer, err *error) { //nolint: gocritic // the pointer of pointer is on purpose so we can change the value if it's nil
 	e := c.Close()
 	if *err == nil && e != nil {
 		*err = e
