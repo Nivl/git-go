a04f9fab9da6596d76c20e4844a2006f58fad5e9
Melvin Laplanche <melvin.wont.reply@gmail.com>
2021-03-22 00:28:55 -0700

    add tests for fsbackend.Init

--- backend/fsbackend/fsbackend_test.go
+++ backend/fsbackend/fsbackend_test.go
@@ -32,6 +32,38 @@ func TestInit(t *testing.T) {
 		require.NoError(t, b.Init())
 	})
 
+	t.Run("repo with separated object dir", func(t *testing.T) {
+		t.Parallel()
+
+		repo, cleanupRepo := testhelper.TempDir(t)
+		t.Cleanup(cleanupRepo)
+
+		gitDir := filepath.Join(repo, gitpath.DotGitPath)
+		objectDir := filepath.Join(repo, "git-objects")
+		b, err := fsbackend.NewWithObjectsPath(gitDir, objectDir)
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			require.NoError(t, b.Close())
+		})
+
+		require.NoError(t, b.Init())
+
+		_, err = os.Stat(gitDir)
+		assert.NoError(t, err)
+
+		// Check the directories that should exists
+		_, err = os.Stat(gitDir)
+		assert.NoError(t, err)
+		_, err = os.Stat(objectDir)
+		assert.NoError(t, err)
+		_, err = os.Stat(filepath.Join(objectDir, gitpath.ObjectsInfoPath))
+		assert.NoError(t, err)
+
+		// Check the directories that should NOT exists
+		_, err = os.Stat(filepath.Join(gitDir, gitpath.ObjectsPath))
+		assert.Error(t, err)
+	})
+
 	t.Run("bare repo should work", func(t *testing.T) {
 		t.Parallel()
 
